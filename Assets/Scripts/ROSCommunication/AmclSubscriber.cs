da24130ac21878f516d7329e10a9b11eeb7a4280
kgandhi09 <kushal009gandhi@gmail.com>
2022-04-18 01:51:33 -0400

    convert pose to twiust

--- Assets/Scripts/ROSCommunication/AmclSubscriber.cs
+++ Assets/Scripts/ROSCommunication/AmclSubscriber.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,6 +18,20 @@ public class AmclSubscriber : MonoBehaviour
     // Variables required for ROS communication
     public string AmclTopicName = "/amcl_pose";
 
+    private double init_x = 0.0;
+    private double init_y = 0.0;
+
+    private double delta_x;
+    private double delta_y;
+
+    private double v_x;
+    private double v_y;
+    private double linear_vel;
+
+    private double init_time;
+    private double curr_time; 
+    private double delta_time;
+
     public ArticulationWheelController wheelController;
     
     void Start()
@@ -25,6 +40,11 @@ public class AmclSubscriber : MonoBehaviour
         ros = ROSConnection.GetOrCreateInstance();
         
         ros.Subscribe<PoseWithCovarianceStampedMsg>(AmclTopicName, updatePose);
+        init_time = Time.time; 
+
+    }
+
+    void LateUpdate(){
     }
 
     private void FixedUpdate()
@@ -34,6 +54,22 @@ public class AmclSubscriber : MonoBehaviour
 
     private void updatePose(PoseWithCovarianceStampedMsg pose)
     {
-        Debug.Log("in update Pose");
+        double x = pose.pose.pose.position.x;
+        double y = pose.pose.pose.position.y;
+
+        delta_x = x - init_x;
+        delta_y = y - init_y;
+
+        curr_time = Time.time;
+        delta_time = init_time-curr_time;
+        init_time = curr_time;
+
+        v_x = delta_x/delta_time;
+        v_y = delta_y/delta_time;
+        linear_vel = Math.Sqrt(v_x*v_x + v_y*v_y);
+
+        Debug.Log(linear_vel);
+
+
     }
 }
