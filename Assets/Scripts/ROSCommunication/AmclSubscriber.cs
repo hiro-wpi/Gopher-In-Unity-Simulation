1732b3601e1f777f4045d6d669eb48a91b9e426e
kgandhi09 <kushal009gandhi@gmail.com>
2022-04-20 19:31:47 -0400

    root teleport working according to subscribed AMCL pose, but need to convert pose correctly

--- Assets/Scripts/ROSCommunication/AmclSubscriber.cs
+++ Assets/Scripts/ROSCommunication/AmclSubscriber.cs
@@ -1,142 +1,69 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-using Unity.Robotics.Core;
+using RosMessageTypes.Geometry;
 using Unity.Robotics.ROSTCPConnector;
 using Unity.Robotics.ROSTCPConnector.ROSGeometry;
-using RosMessageTypes.Geometry;
+using RosMessageTypes.Sensor;
 
 /// <summary>
-///     This script subscribes to twist command
-///     and use robot controller to 
+///     This script subscribes and display an image
+///     sent from ROS.
 /// </summary>
 public class AmclSubscriber : MonoBehaviour
 {
     // ROS Connector
     private ROSConnection ros;
     // Variables required for ROS communication
-    public string AmclTopicName = "/amcl_pose";
-    public string AmclBotTopicName = "/pose_unity_bot";
+    public string AmclTopicName = "amcl_pose";
 
-    private TwistStampedPublisher twistStampedPublisher;
-    private TwistMsg twistMsg;
+    // Body to move
+    public ArticulationBody articulationBody;
+    
+    // Message info
+    private PoseWithCovarianceMsg message;
+    private PoseMsg pose;
+    private bool isMessageReceived;
 
-    private Vector3 unity_coords;
-
-    private double init_x = 0.0;
-    private double init_y = 0.0;
-
-    private double delta_x;
-    private double delta_y;
-
-    private double v_x;
-    private double v_y;
-    private double twist_x;
-    private double twist_y;
-
-    private double init_time;
-    private double curr_time; 
-    private double delta_time;
-
-    //making sphere gameobject for testing
     private GameObject test_sphere;
-
-    public ArticulationWheelController wheelController;
-
-    struct EulerAngles{
-        public double roll;
-        public double pitch; 
-        public double yaw;
-    }
+    private Vector3 unity_coords;
 
     void Start()
     {
         // Get ROS connection static instance
         ros = ROSConnection.GetOrCreateInstance();
-        //ros.RegisterPublisher<Pose>(TwistTopicName);
-        
-        // Initialize message
-        twistMsg = new TwistMsg
-        {
-            // linear
-        };
-        
-        //initializing sphere gameobject
-        // test_sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        // test_sphere.transform.localScale = new Vector3(1.0f ,1.0f ,1.0f);
 
+        // Display
+        message = new PoseWithCovarianceMsg();
+        // meshRenderer.material = new Material(Shader.Find("Standard"));
 
         unity_coords = new Vector3(0.0f, 0.0f, 0.0f);
+        test_sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 
-        ros.Subscribe<PoseWithCovarianceStampedMsg>(AmclTopicName, updatePose);
+        // Subscriber
+        ros.Subscribe<PoseWithCovarianceMsg>(AmclTopicName, ReceiveAmcl);
+        isMessageReceived = false;
         
-        init_time = Time.time;
     }
 
-    void LateUpdate(){
-    }
-
-    private void FixedUpdate()
+    void Update()
     {
-        // wheelController.SetRobotVelocity(targetLinearVelocity, targetAngularVelocity);
+        // Display image if received
+        if (isMessageReceived)
+        {
+            Vector3Msg vec = new Vector3Msg(pose.position.x,pose.position.y,pose.position.z);
+            //unity frame .To<FLU>() //teleport body
+            articulationBody.TeleportRoot(vec.From<FLU>(),pose.orientation.From<FLU>());
+
+            isMessageReceived = false;
+
+        }
     }
 
-    private void updatePose(PoseWithCovarianceStampedMsg pose) 
+    private void ReceiveAmcl(PoseWithCovarianceMsg amcl)
     {
-        double x = pose.pose.pose.position.x;
-        double y = pose.pose.pose.position.y;
-
-        EulerAngles angles = quaternion_to_euler(pose.pose.pose.orientation.w, pose.pose.pose.orientation.x, pose.pose.pose.orientation.y, pose.pose.pose.orientation.z);
-
-        delta_x = x - init_x;
-        delta_y = y - init_y;
-
-        curr_time = Time.time;
-        delta_time = init_time-curr_time; 
-        init_time = curr_time;
-
-        v_x = delta_x/delta_time;
-        v_y = delta_y/delta_time;
-
-        twist_x = v_x;
-        twist_y = v_y;
-
-        unity_coords.x = (float)(-1.0*y);
-        unity_coords.z = (float)(-1.0*x);
-
-        // Debug.Log(unity_coords);
-
-        // gameObject.transform.parent.transform.parent.position = unity_coords;
-        // test_sphere.transform.position = unity_coords;
-        // Debug.Log(test_sphere.transform.position);
-
+        pose = amcl.pose;
+        isMessageReceived = true; 
     }
-
-    private EulerAngles quaternion_to_euler(double w, double x, double y, double z){
-        EulerAngles angles;
-
-        double sinr_cosp = 2*(w*x + y*z);
-        double cosr_cosp = 1 - (2*(x*x + y*y));
-        angles.roll = Math.Atan2(sinr_cosp, cosr_cosp);
-
-        double sinp = 2*(w*y - z*x);
-        if(sinp > 1){
-            sinp = 1;
-        }
-        if(sinp < -1){
-            sinp = -1;
-        }
-        angles.pitch = Math.Asin(sinp);
-
-        double siny_cosp = 2*(w*z + x*y);
-        double cosy_cosp = 1 - 2*(y*y + z*z);
-        angles.yaw = Math.Atan2(siny_cosp, cosy_cosp);
-
-        return angles;
-
-    }
-
 }
