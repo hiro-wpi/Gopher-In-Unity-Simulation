2c6a88de7fb7e8c4b3135d8c0cb0232795180fbc
Robear Mankaryous <rmankaryous12@gmail.com>
2022-05-05 11:23:28 -0400

    attempt to fix vr hands

--- Assets/Scripts/Interface/Nurse/HandController.cs
+++ Assets/Scripts/Interface/Nurse/HandController.cs
@@ -56,31 +56,43 @@ public class HandController : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 direction = target.position - rBody.worldCenterOfMass;
-        float maxSpeed = Mathf.Min(speed, direction.magnitude * speed);
 
-        // cancel out existing velocity
-        rBody.velocity = Vector3.zero;
-        // rBody.AddForce(-rBody.velocity, ForceMode.VelocityChange);
-        // then add new force
-        rBody.AddForce(direction.normalized * totalMass * maxSpeed);
-
-        Vector3 axis = Vector3.Cross(transform.forward, target.forward);
-        float maxRotation = Mathf.Min(rotationSpeed, axis.magnitude * rotationSpeed);
-
-        // cancel out existing angular velocity
-        rBody.angularVelocity = Vector3.zero;
-        // rBody.AddTorque(-rBody.angularVelocity, ForceMode.VelocityChange);
-        // add new angular velocity
-        rBody.AddTorque(axis * totalMass * maxRotation);
-
-        axis = Vector3.Cross(transform.up, target.up);
-        rBody.AddTorque(axis * totalMass * maxRotation);
-
-        foreach (ArticulationBody arBody in fingers)
+        // If the distance is close enough,
+        if (direction.magnitude < 0.5f)
         {
-            ArticulationDrive xDrive = arBody.xDrive;
-            xDrive.target = Mathf.Lerp(rangeMin, rangeMax, grip);
-            arBody.xDrive = xDrive;
+            float maxSpeed = Mathf.Min(speed, direction.magnitude * speed);
+
+            // cancel out existing velocity
+            rBody.velocity = Vector3.zero;
+            // rBody.AddForce(-rBody.velocity, ForceMode.VelocityChange);
+            // then add new force
+            rBody.AddForce(direction.normalized * totalMass * maxSpeed);
+
+            Vector3 axis = Vector3.Cross(transform.forward, target.forward);
+            float maxRotation = Mathf.Min(rotationSpeed, axis.magnitude * rotationSpeed);
+
+            // cancel out existing angular velocity
+            rBody.angularVelocity = Vector3.zero;
+            // rBody.AddTorque(-rBody.angularVelocity, ForceMode.VelocityChange);
+            // add new angular velocity
+            rBody.AddTorque(axis * totalMass * maxRotation);
+
+            axis = Vector3.Cross(transform.up, target.up);
+            rBody.AddTorque(axis * totalMass * maxRotation);
+
+            foreach (ArticulationBody arBody in fingers)
+            {
+                ArticulationDrive xDrive = arBody.xDrive;
+                xDrive.target = Mathf.Lerp(rangeMin, rangeMax, grip);
+                arBody.xDrive = xDrive;
+            }
         }
+        else
+        {
+            // Teleport to the target
+            Debug.Log("Teleport");
+            rBody.TeleportRoot(target.position, target.rotation);
+        }
+
     }
 }
