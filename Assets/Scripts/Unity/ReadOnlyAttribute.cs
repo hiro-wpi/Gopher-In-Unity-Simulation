eec987c27734ce15ad80e50e02caba736d0b6a92
ZhuoyunZhong <zzy905954450@gmail.com>
2023-03-15 12:26:49 -0400

    add Unity UI attribute

---
+++ Assets/Scripts/Unity/ReadOnlyAttribute.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+///    This attribute is used to make a variable 
+///    read-only in the inspector
+/// </summary>
+public class ReadOnlyAttribute : PropertyAttribute
+{
+    
+}
---
+++ Assets/Scripts/Unity/ReadOnlyDrawer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+/// <summary>
+///    This drawer is used to make a variable 
+///    read-only in the inspector
+/// </summary>
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(
+        SerializedProperty property, GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
+
+    public override void OnGUI(
+        Rect position, SerializedProperty property, GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
