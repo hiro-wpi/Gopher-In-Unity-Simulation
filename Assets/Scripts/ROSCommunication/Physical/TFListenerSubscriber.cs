f4802ce9e283e11ccb4c4837ca8ae97941d87ced
miyengar26 <hoangtheshark2@gmail.com>
2023-06-14 16:43:19 -0400

    tf listener dev further, needs testing

--- Assets/Scripts/ROSCommunication/Physical/TFListenerSubscriber.cs
+++ Assets/Scripts/ROSCommunication/Physical/TFListenerSubscriber.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using Unity.Robotics.ROSTCPConnector;
-using RosMessageTypes.Std;
+using Unity.Robotics.ROSTCPConnector.ROSGeometry;
 using RosMessageTypes.Geometry;
-using RosMessageTypes.GopherNavigation
+using RosMessageTypes.GopherNavigation;
 
 /// <summary>
 ///     This scripts focuses on interfacing with the tf listener object 
@@ -13,19 +13,35 @@ using RosMessageTypes.GopherNavigation
 ///     of the needed transformation to initciate the node. This script will
 ///     act as a subscriber and listen in for the transform.
 /// </summary>
-public class MoveBaseCancelGoalService : MonoBehaviour
+public class TFListenerSubscriber : MonoBehaviour
 {
     // ROS Connector
     private ROSConnection ros;
-    // Variables required for ROS communicationS
+
+    // Variables required for ROS communication
     [SerializeField] private string tfListenerServiceName = 
         "tfListener/SetChildAndParentFrames";
     [SerializeField] private string tfListenerTopicName = 
         "tfListener/...";
 
+    // Varibles required for the transform
+    [SerializeField] private string parentFrame = 
+        "/map";
+    [SerializeField] private string childFrame = 
+        "/gopher/base_link";
+
+    public Vector3 position { get; private set; }
+    public Vector3 rotationEuler { get; private set; }
+    public Quaternion rotationQuaternion { get; private set; }
+
     // Message
-    private TFListenerServiceRequest request;
-    private TFListenerServiceResponse response;
+    private TFListenerServiceRequest tfListenerServiceRequest;
+    // private TFListenerServiceResponse tfListenerServicerResponse;
+
+    // Transform 
+    private TransformStampedMsg transformMsg;
+    private bool isValidTransform;
+
 
     void Start()
     {
@@ -34,24 +50,58 @@ public class MoveBaseCancelGoalService : MonoBehaviour
         ros.RegisterRosService<TFListenerServiceRequest, TFListenerServiceResponse>(
             tfListenerServiceName
         );
-        ros.Subscriber(tfListenerTopicName, topicCallback);
+
+        ros.Subscribe<TransformStampedMsg>(tfListenerTopicName, tfListenerCallback);
 
         // Initialize service request
-        emptyRequest = new EmptyRequest();
+        tfListenerServiceRequest = new TFListenerServiceRequest();
+
+        SetFramesCommand();
     }
 
     void Update() {}
 
-    public void topicCallback(TransformStampedMsg)
-
-    // Request service to cancel all goal(s) to move base
-    public void CancelGoalCommand()
+    // Handles the published transformation msgs from the tfListener Node
+    public void tfListenerCallback(TransformStampedMsg msg)
     {
+        if(isValidTransform)
+        {
+            // Check if the parent and child are the same (should be)
+            if(parentFrame == msg.header.frame_id & childFrame == msg.child_frame_id)
+            {
+                position = msg.transform.translation.From<FLU>();
+                rotationQuaternion = msg.transform.rotation.From<FLU>();
+                rotationEuler = msg.transform.rotation.From<FLU>().eulerAngles;
+            }
+            else
+            {
+                Debug.LogError("Requested Transformation Frames Does Not Match Recieved Frames. Ignoring Transformation");
+            }
+        }
+        else
+        {
+            Debug.Log("Not a valid transform");
+        }
+            
+    }
+
+    // Request Service to Start Sending Transform
+    public void SetFramesCommand()
+    {
+        // Init Msg
+        tfListenerServiceRequest = new TFListenerServiceRequest(
+            new string(parentFrame),
+            new string(childFrame)
+        );
+
         // Request service
-        ros.SendServiceMessage<EmptyResponse>(
-            moveBaseCancelGoalServiceName, emptyRequest, CancelGoalCallback
+        ros.SendServiceMessage<TFListenerServiceResponse>(
+            tfListenerServiceName, tfListenerServiceRequest, SetFramesCallback
         );
     }
 
-    private void CancelGoalCallback(EmptyResponse response) {}
+    private void SetFramesCallback(TFListenerServiceResponse response) 
+    {
+        isValidTransform = response.isValidTransform;        
+    }
 }
