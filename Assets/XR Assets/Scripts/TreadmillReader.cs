676c6fe21153d0fd4a9a73a33e272f01d6ffc7d8
ZhuoyunZhong <zzhong3@wpi.edu>
2023-11-19 20:27:55 -0500

    Update treadmill reading

--- Assets/XR Assets/Scripts/TreadmillReader.cs
+++ Assets/XR Assets/Scripts/TreadmillReader.cs
@@ -2,38 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+///    Read the velocity and orientation of the
+///    Kat Walk treadmill
+/// </summary>
 public class TreadmillReader : MonoBehaviour
 {
-    [SerializeField] private float smoothTime = 0.2f; // Adjust this value for smoother or quicker dampening
-    
-    private Vector2 currentVelocity = new Vector2(0.0f, 0.0f); // Store the current velocity for SmoothDamp
+    [SerializeField] 
+    private Vector2 velocityScale = new Vector2(1.0f, 1.0f);
+    [SerializeField, ReadOnly] 
+    private Vector2 currentVelocity = Vector2.zero;
+    [SerializeField, ReadOnly] 
+    private float rotation = 0.0f;
 
-    [SerializeField] private Vector2 velocityScale = new Vector2(1.0f, 1.0f);
-
-    [SerializeField] private Vector2 velocityXZ = Vector2.zero;
-
-    [SerializeField] private float rotationY = 0.0f;
+    private Vector2 smoothVelocity = new Vector2(0.0f, 0.0f);
+    private float smoothRotation = 0.0f;
 
     void Update()
     {
+        // Get velocity from the Kat Walk treadmill
         var ws = KATNativeSDK.GetWalkStatus();
-
         Vector2 targetVelocity = new Vector2(ws.moveSpeed.x, ws.moveSpeed.z);
+        float targetRotation = ws.bodyRotationRaw.eulerAngles.y;
 
-        // Apply smoothing using Vector2.SmoothDamp()
-        velocityXZ = Vector2.SmoothDamp(velocityXZ, targetVelocity, ref currentVelocity, smoothTime);
-
-        // Smooth this
-        // rotationY = ws.bodyRotationRaw.eulerAngles.y;
+        // Apply smoothing
+        currentVelocity = Vector2.SmoothDamp(
+            currentVelocity, targetVelocity, ref smoothVelocity, 0.1f
+        );
+        rotation = Mathf.SmoothDamp(
+            rotation, targetRotation, ref smoothRotation, 0.1f
+        );
     }
 
     public Vector2 GetVelocity()
     {
-        return velocityXZ * velocityScale;
+        return currentVelocity * velocityScale;
     }
 
     public float GetRotation()
     {
-        return rotationY;
+        return rotation;
     }
 }
