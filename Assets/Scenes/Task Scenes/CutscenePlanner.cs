f1f4b7b6153070593802480e8f49be299cdd209e
Maanav Iyengar <miyengar@wpi.edu>
2024-01-30 16:57:03 -0500

    Finished random medicine selection and updated animator controller to include hand animations

--- Assets/Scenes/Task Scenes/CutscenePlanner.cs
+++ Assets/Scenes/Task Scenes/CutscenePlanner.cs
@@ -23,9 +23,10 @@ public class CutscenePlanner : MonoBehaviour
     {
         GoingToHome,
         WaitingBeforeMedicine,
+        SelectingRandomMedicine,
         GoingToMedicine,
         WaitingAtMedicine,
-        StayAtHome
+        StayingAtHome
     }
 
     private CutsceneState currentState;
@@ -43,78 +44,34 @@ public class CutscenePlanner : MonoBehaviour
         {
             case CutsceneState.GoingToHome:
                 goToHome();
-                if (isAtDestination(leftHandHome.position))
-                {
-                    Debug.Log("Switching state to WaitingBeforeMedicine");
-                    currentState = CutsceneState.WaitingBeforeMedicine;
-                }
                 break;
 
             case CutsceneState.WaitingBeforeMedicine:
                 waitBeforeMedicine();
-                Debug.Log("Finished waiting for medicine");
+                break;
+
+            case CutsceneState.SelectingRandomMedicine:
+                selectRandomMedicine();
                 break;
 
             case CutsceneState.GoingToMedicine:
-                Debug.Log("About to go to medicine");
                 goToMedicine();
-                Debug.Log("Exited goToMedicine()");
                 break;
 
             case CutsceneState.WaitingAtMedicine:
-                Debug.Log("Made it to WaitingAtMedicine!");
                 waitAtMedicine();
                 break;
 
-            case CutsceneState.StayAtHome:
+            case CutsceneState.StayingAtHome:
                 goToHome();
                 break;
         }
     }
-
-    private void goToHome()
-    {
-        leftHandTarget.position = Vector3.Lerp(leftHandTarget.position, leftHandHome.position, 1.0f * Time.deltaTime);
-    }
-
-    // go to a random medicine in the list and remove it from the list after it has been visited
-    // if there are no more medicines in the list, go to home
-    private void goToMedicine()
-    {
-        Debug.Log("medicineBoxes.count: " + medicineBoxes.Count);
-
-        if (medicineBoxes.Count > 0)
-        {            
-            randomMedicine = Random.Range(0, medicineBoxes.Count - 1);
-            Debug.Log("randomMedicine: " + randomMedicine);
-            
-            leftHandTarget.position = Vector3.Lerp(leftHandTarget.position, medicineBoxes[randomMedicine].position, 1.0f * Time.deltaTime);
-            Debug.Log("Going to medicine");
-
-            if (isAtDestination(medicineBoxes[randomMedicine].position))
-            {
-                Debug.Log("Removed medicine from list");
-                medicineBoxes.RemoveAt(randomMedicine);
-
-                Debug.Log("Reached medicine and switching to WaitingAtMedicine");
-                currentState = CutsceneState.WaitingAtMedicine;
-            }
-        }
-
-        else 
-        {
-            currentState = CutsceneState.StayAtHome;
-        }
-    }
-
-    private bool isAtDestination(Vector3 destination)
-    {
-        return Vector3.Distance(leftHandTarget.position, destination) < 0.01f;
-    }
-
+    
     private void waitAtMedicine()
     {
         holdTimer += Time.deltaTime;
+
         if (holdTimer >= holdDuration)
         {
             currentState = CutsceneState.GoingToHome;
@@ -122,14 +79,54 @@ public class CutscenePlanner : MonoBehaviour
         }
     }
 
+    private void goToMedicine()
+    {
+        leftHandTarget.position = Vector3.Lerp(leftHandTarget.position, medicineBoxes[randomMedicine].position, 1.0f * Time.deltaTime);
+
+        if (isAtDestination(medicineBoxes[randomMedicine].position))
+        {
+            medicineBoxes.RemoveAt(randomMedicine);
+            currentState = CutsceneState.WaitingAtMedicine;
+        }
+    }
+
+    private void selectRandomMedicine()
+    {
+        if (medicineBoxes.Count > 0)
+        {            
+            randomMedicine = Random.Range(0, medicineBoxes.Count - 1);
+            currentState = CutsceneState.GoingToMedicine;
+        }
+        else
+        {
+            currentState = CutsceneState.StayingAtHome;
+        }
+    }
+
     private void waitBeforeMedicine()
     {
         waitTimer += Time.deltaTime;
+
         if (waitTimer >= Random.Range(minWait, maxWait))
         {
-            Debug.Log("Switching state to GoingToMedicine");
             currentState = CutsceneState.GoingToMedicine;
+            selectRandomMedicine();
             waitTimer = 0.0f;
         }
+    }
+
+    private void goToHome()
+    {
+        leftHandTarget.position = Vector3.Lerp(leftHandTarget.position, leftHandHome.position, 1.0f * Time.deltaTime);
+
+        if (isAtDestination(leftHandHome.position))
+        {
+            currentState = CutsceneState.WaitingBeforeMedicine;
+        }
+    }
+
+    private bool isAtDestination(Vector3 destination)
+    {
+        return Vector3.Distance(leftHandTarget.position, destination) < 0.01f;
     }
 }
