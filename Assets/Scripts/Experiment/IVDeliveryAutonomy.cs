135047fd8a26309f95ea6f4417d4a9ff963be23f
Hoang Nguyen <hoangtheshark2@gmail.com>
2024-02-14 21:01:28 -0500

    Task Tested With VR Controllers
    - Obsticle and IV are XR graspable now

--- Assets/Scripts/Experiment/IVDeliveryAutonomy.cs
+++ Assets/Scripts/Experiment/IVDeliveryAutonomy.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class IVDeliveryAutonomy : MonoBehaviour
 {
     // Test Setup (General)
-    // [SerializeField] private GraphicalInterface graphicalInterface;
-    // [SerializeField] private RectTransform displayRect;
-    // private Camera cam;
+    [SerializeField] private GraphicalInterface graphicalInterface;
+    [SerializeField] private RectTransform displayRect;
+    private Camera cam;
     private GameObject robot;
+
+    // AR
+    [SerializeField] private GenerateARGameObject arGenerator;
+    [SerializeField] private HighlightObjectOnCanvas highlightObject;
+    [SerializeField] private Sprite icon;
 
     // Autonomy
 
@@ -49,16 +54,16 @@ public class IVDeliveryAutonomy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Get the main camera if it is not already set
-        // if (cam == null)
-        // {
-        //     // Get all the active cameras referanced in the graphical interface
-        //     Camera[] cameras =  graphicalInterface.GetCurrentActiveCameras();
-        //     if (cameras.Length > 0)
-        //     {
-        //         cam = cameras[0];
-        //     }
-        // }
+        //Get the main camera if it is not already set
+        if (cam == null)
+        {
+            // Get all the active cameras referanced in the graphical interface
+            Camera[] cameras =  graphicalInterface.GetCurrentActiveCameras();
+            if (cameras.Length > 0)
+            {
+                cam = cameras[0];
+            }
+        }
 
         if(robot == null)
         {
@@ -70,6 +75,16 @@ public class IVDeliveryAutonomy : MonoBehaviour
                 // Debug.Log("No robot found");
                 return;
             }
+
+            StartCoroutine(ChangeControlInterface());
+
+            // GameObject controlInterfaces = GameObject.Find("Control Interface");
+
+            // Transform keyboardControl = controlInterfaces.transform.Find("Full Keyboard Control");
+            // Transform xrControl = controlInterfaces.transform.Find("XR Control");
+
+            // keyboardControl.gameObject.SetActive(false);
+            // xrControl.gameObject.SetActive(true);
         }
 
         InitTaskSetup();
@@ -101,6 +116,21 @@ public class IVDeliveryAutonomy : MonoBehaviour
         //     arNavAuto.SetWaypoints(posTraj, rotTraj);
         // }
 
+    }
+
+    IEnumerator ChangeControlInterface()
+    {
+        yield return new WaitForSeconds(10);
+
+        GameObject controlInterfaces = GameObject.Find("Control Interface");
+
+        Transform keyboardControl = controlInterfaces.transform.Find("Full Keyboard Control");
+        Transform xrControl = controlInterfaces.transform.Find("XR Control");
+
+        keyboardControl.gameObject.SetActive(false);
+        xrControl.gameObject.SetActive(true);
+
+        Debug.Log("Swaped Controls");
     }
 
     public void InitTaskSetup()
@@ -135,7 +165,11 @@ public class IVDeliveryAutonomy : MonoBehaviour
             }
             else
             {
+                // Set the init position of the obsticle
                 obsticleInitPosition = obsticle.transform.position;
+
+                // Create a box collider for the obsticle
+                AddCartCollider();
             }
         }
 
@@ -156,7 +190,7 @@ public class IVDeliveryAutonomy : MonoBehaviour
                     controlState = ControlState.ManualToAuto;
                     SetTaskWaypoints();
                 }
-                
+
                 break;
             case ControlState.ManualToAuto:
                 if(Input.GetKeyUp(KeyCode.Space))
@@ -200,9 +234,10 @@ public class IVDeliveryAutonomy : MonoBehaviour
         {
             case ObsticleState.None:
                 // Change the state if the robot is close to the obsticle
-                if(RobotDistanceToPosition(obsticle.transform.position) < 1.5f)
+                if(ObsticleDetected())
                 {
                     Debug.LogWarning("Obsticle Detected");
+                    CreateCartAR(obsticle);
                     obsticleState = ObsticleState.Detected;
                 }
                 break;
@@ -213,6 +248,7 @@ public class IVDeliveryAutonomy : MonoBehaviour
                 if(Vector3.Distance(obsticle.transform.position, obsticleInitPosition) > 0.5f)
                 {
                     Debug.LogWarning("Obsticle Handled");
+                    DestoryCartAR(obsticle);
                     obsticleState = ObsticleState.Handled;
                 }
                 break;
@@ -278,6 +314,45 @@ public class IVDeliveryAutonomy : MonoBehaviour
         }
     }
 
+    private bool ObsticleDetected()
+    {
+        // Use a raycast from the robot to the cart to determine if we need to change state
+        
+        RaycastHit hit;
+        Vector3 direction = obsticle.transform.position - robot.transform.position;
+        float maxDistance = Vector3.Distance(obsticle.transform.position, robot.transform.position);
+
+        Debug.DrawRay(robot.transform.position + Vector3.up * 0.25f , direction,  Color.red);
+
+        if(Physics.Raycast(robot.transform.position + Vector3.up * 0.25f, direction, out hit, maxDistance))
+        {
+            // No obsticle detected between the robot cart
+            // Check if the robot is closer to the doorfront or doorback to determine if the robot is in the room or not
+
+            // Check that hit object is the cart
+            Debug.Log(hit.collider.gameObject.name);
+
+            // Transform child = obsticle.transform.Find(hit.collider.gameObject.name);
+
+            // if(child == null)
+            // {
+            //    return false;
+            // }
+            // else
+            // {
+            //     Debug.Log("Child Found");
+            //     return true;
+            // }
+            if(hit.collider.gameObject == obsticle)
+            {
+                Debug.Log("Obsticle Detected");
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public float RobotDistanceToPosition(Vector3 position)
     {
         // Get the distance from the robot to the given position
@@ -308,6 +383,80 @@ public class IVDeliveryAutonomy : MonoBehaviour
         doorBack.transform.localPosition = offset;
         doorBack.name = "DoorBack";
 
+    }
+
+    private void CreateCartAR(GameObject selectedObject)
+    {
+        // Create the 3D AR object
+        // if(selectedObject == null)
+        // {
+        //     Debug.Log("No selected Object Found");
+        //     return;
+        // }
+
+        // if(positionOffset == null)
+        // {
+        //     positionOffset = Vector3.zero;
+        // }
+        
+        var type = GenerateARGameObject.ARObjectType.Cube;
+        arGenerator.Instantiate(
+            selectedObject,
+            type,
+            new Vector3(0, 0.45f, 0),
+            new Vector3(0, 0, 0),
+            new Vector3(0.6f, 0.9f, 0.9f),
+            Color.red,
+            0.25f
+        );
+
+        // Create the 2D AR object
+
+        // Create a cube gameobject as a child of the selected object
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.SetParent(selectedObject.transform);
+        cube.transform.localPosition = new Vector3(0, 1.1f, 0);
+        cube.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);   // Scale will affect the size of the highlight
+        cube.name = "2D Highlight";
+
+        // Disable the collider and render of the cube
+        cube.GetComponent<Collider>().enabled = false;
+        cube.GetComponent<Renderer>().enabled = false;
+
+        var location = HighlightObjectOnCanvas.ElementPosition.Center;
+
+        highlightObject.Highlight(
+            cube,
+            cam,
+            displayRect,
+            icon,
+            Color.white,
+            adjustUIScale: false,
+            position: location
+        );
+    }
+
+    private void DestoryCartAR(GameObject selectedObject)
+    {
+        // Destroy the 3D AR object
+        arGenerator.Destroy(selectedObject);
+
+        // Destroy the 2D AR object
+
+        // Find the cube
+        var cube = selectedObject.transform.Find("2D Highlight").gameObject;
+
+        // Destroy the previous highlight
+        highlightObject.RemoveHighlight(cube);
+    }
+
+    private void AddCartCollider()
+    {
+        // Add a box collider to the cart
+        BoxCollider boxCollider = obsticle.AddComponent<BoxCollider>();
+        boxCollider.center = new Vector3(0, 0.5f, 0);
+        boxCollider.size = new Vector3(0.5f, 0.8f, 0.8f);
+        
     }
 
 
