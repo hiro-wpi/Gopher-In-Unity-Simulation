c97f6e3f6859939335da4921374bfb98f006a23d
Maanav Iyengar <miyengar@wpi.edu>
2024-02-19 01:58:08 -0500

    data recorded and successfully sent to a csv file

--- Assets/Scenes/Manipulation Task/ManipulationTaskDataRecorder.cs
+++ Assets/Scenes/Manipulation Task/ManipulationTaskDataRecorder.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,9 @@ public class ManipulationTaskDataRecorder : MonoBehaviour
     [SerializeField] private GraspingTask graspingTask;
     bool isDataRecorded = false;
     private string dataString = "";
-
+    public string fileName;
+    private TextWriter textWriter;
+    
     private void Update()
     {
         // if the task is completed, print the task duration and the number of collisions
@@ -17,6 +20,9 @@ public class ManipulationTaskDataRecorder : MonoBehaviour
             isDataRecorded = true;
             dataString = "Completion Time: " + graspingTask.taskDuration + " seconds, Number of Collisions: " + robotCollisionWarning.collisionCounter;
             Debug.Log(dataString);
+            textWriter = new StreamWriter(fileName + "_manipulation_task.csv", false);
+            textWriter.WriteLine(dataString);
+            textWriter.Close();
         }
     }
 }
